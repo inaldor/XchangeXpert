c613a3c02724be67612f80ed445ba8aa0bad93a4
Inaldo Ramos Ribeiro <inaldor@bibimoney.com>
2020-03-30 23:34:26 +0100

    task completed

--- XchangeXpert/Controller/CurrenciesTableViewController.swift
+++ XchangeXpert/Controller/CurrenciesTableViewController.swift
@@ -13,14 +13,24 @@ class CurrenciesTableViewController: UIViewController, UITableViewDelegate, UITa
     var currencies: [String] = []
     var selectedCurrencies: [String] = []
     //var cellsSelected = [Cell]()
-    
+ 
+    let userDefaults = UserDefaults.standard
+    var udArray = UserDefaults.standard.stringArray(forKey: "currencies_selected") ?? []
+    //var  = userDefaults.
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //tableView.allowsMultipleSelection = true
         //tableView.allowsMultipleSelectionDuringEditing = true
-        
+    
+        fetchListCurrencies()
+        //fetchData()
+                
+    }
+    
+    fileprivate func fetchListCurrencies() {
+
         do {
            let jsonURL = Bundle.main.url(forResource: "currencies", withExtension: "json")
            let jsonDecoder = JSONDecoder()
@@ -34,29 +44,6 @@ class CurrenciesTableViewController: UIViewController, UITableViewDelegate, UITa
            print(error)
         }
         
-     
-        fetchItems()
-        fetchData()
-                
-    }
-    
-    fileprivate func fetchItems() {
-//        Service.shared.fetchItems { (items, error) in
-//            if let error = error {
-//                print("Failed to fetch items:", error)
-//                return
-//            }
-//
-//            guard let ite = items else { return }
-//
-//            self.itemsArray =  ite.items.map( { $0 } )
-//
-//            self.itemsArray = self.itemsArray.sorted(by: { $0.id < $1.id })
-//
-//            self.itemUnique?.id = 32
-//
-//            self.tableView.reloadData()
-//        }
     }
     
     fileprivate func fetchData() {
@@ -129,6 +116,15 @@ class CurrenciesTableViewController: UIViewController, UITableViewDelegate, UITa
 //
 //        }
 //
+//
+//        let userDefaults = UserDefaults.standard
+//        let value = userDefaults.stringArray(forKey: "currencies_selected") ?? []
+//        print(value)
+//
+        //print(selectedCurrencies)
+        
+        
+        
         
         let currencyName = currencies[indexPath.row]
         
@@ -141,6 +137,8 @@ class CurrenciesTableViewController: UIViewController, UITableViewDelegate, UITa
             selectedCurrencies.append(currencies[indexPath.row])
             
         }
+        
+        print(selectedCurrencies)
         
         if selectedCurrencies.count == 2 {
             
@@ -160,15 +158,67 @@ class CurrenciesTableViewController: UIViewController, UITableViewDelegate, UITa
 //            urls.append(currenciesCombined)
 //            usD.intArray
             
-            let userDefaults = UserDefaults.standard
-            var udArray = userDefaults.stringArray(forKey: "currencies_selected") ?? []
             udArray.append(currenciesCombined)
             userDefaults.set(udArray, forKey: "currencies_selected")
             
             /// Pushing a new ViewController
             self.navigationController?.popToRootViewController(animated: true)
-        }
+            
+        } else if selectedCurrencies.count == 1 {
+            
+            guard let lastSelectedCurrency = selectedCurrencies.last else { return }
+                        
+            print(lastSelectedCurrency)
+            
+            //let ok = udArray.dropLast(3)
+            
+            //print(ok)
+            
+            let result = udArray.filter({ $0.dropLast(3) == lastSelectedCurrency })
+            
+            //contains(where: { $0.dropLast(3) == "BGN" })
+            
+            //let ok = ["BGN","IDR"]
+            
+            print(result)
+            
+            //et ok = result.map( { $0 })
+            
+            //let ok = result.map( { })
+              
+            //print(ok)
+                
+            //let sdok = currencies.filter({ $0.dropLast(3) != ok })
+            
+            for r in result {
+                print(r)
+                
+                guard let index = currencies.lastIndex(of: String(r.dropFirst(3))) else { return }
+                
+                currencies = currencies.filter({ $0 != r.dropFirst(3) })
+                
+                print(currencies)
+                
+                let indexPathrow = IndexPath(item: index, section: 0)
+                tableView.deleteRows(at: [indexPathrow], with: .fade)
+                
+                print(index)
+            }
+                
+            //currencies = currencies.filter({ $0.dropLast(3) != result })
+            
+            //currencies = currencies.filter { !result.contains($0) }
+            
+            print(currencies)
+            
+//            if udArray.dropLast(3).contains(selectedCurrencies.last!) {
+//                print("possui")
+//            } else {
 //
+//                print("nao possui")
+//            }
+            
+        }
 //        let ok = itemsArray[indexPath.row].id
 //
 //        let vc = ItemDetailViewController()
@@ -183,6 +233,9 @@ class CurrenciesTableViewController: UIViewController, UITableViewDelegate, UITa
         let currencyName = currencies[indexPath.row]
         
         selectedCurrencies = selectedCurrencies.filter { $0 != currencyName }
+        
+        fetchListCurrencies()
+        tableView.reloadData()
         
         print(selectedCurrencies)
     }
--- XchangeXpert/Controller/HomeViewController.swift
+++ XchangeXpert/Controller/HomeViewController.swift
@@ -12,124 +12,56 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var addCurrencyPairButton: UIButton!
     
-    //var rates: [String:Double] = [:]
     var rates: [Double] = []
     var currencyBase: [String] = []
     var currencyOutput: [String] = []
     
-    //let myTableView = UITableView()
-    /// Variable to store the ID of the selected item
-    //var currencyPairs: [String:Double] = [:]
+    var noRepeatControl = false
+    var loadTableViewControl = true
+    var timer: Timer!
     
     override func viewDidAppear(_ animated: Bool) {
         
-        fetchRates()
+        noRepeatControl = false
+        
+        loadTableViewControl = true
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.loop), userInfo: nil, repeats: true)
 
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //myTableView.register(HomeTableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
-        
-        //self.myTableView.delegate = self
-        //self.myTableView.dataSource = self
-        
-        //myTableView.delegate = self
-        //myTableView.dataSource = self
-        //tableView.delegate = self
-        //tableView.dataSource = self
-        
-        //guard let currenciesSelected = UserDefaults.currenciesSelected? else { return }
-        
-        //print(currenciesSelected)
-        
-        //print(UserDefaults.standard.intArray)
-
-        
-//        do {
-//            let jsonURL = Bundle.main.url(forResource: "currencies", withExtension: "json")
-//            let jsonDecoder = JSONDecoder()
-//            let jsonData = try Data(contentsOf: jsonURL!)
-//            let jsonSentence = try jsonDecoder.decode([String].self, from: jsonData)
-//            currencies = jsonSentence
-//            //debugPrint(currencies[1])
-//            //print(currencies.count)
-//
-//        } catch {
-//            print(error)
-//        }
-
-//        do {
-//            let data = try Data(contentsOf: Bundle.main.url(forResource: "currencies", withExtension: "json")!)
-//                do {
-//                    currencies = try JSONDecoder().decode(Currencies.self, from: data)
-//                } catch {
-//                    print("Failed to decode:")
-//                }
-//        } catch {
-//            print("ok")
-//        }
-//        do {
-//           let data = try Data(contentsOf: Bundle.main.url(forResource: "currencies", withExtension: "json")!)
-//            currencies = try JSONDecoder().decode([Currencies].self, from: data)
-//            //tableView.reloadData()
-//        } catch {
-//            print(error)
-//        }
-//
-        
-//        init(from decoder: Decoder) throws {
-//            var container = try decoder.unkeyedContainer()
-//            let values = try container.decode([String].self)
-//            self.isoCode = values
-//        }
-        
-//        if let url = Bundle.main.url(forResource: currencies, withExtension: "json") {
-//            do {
-//                let data = try Data(contentsOf: url)
-//                let decoder = JSONDecoder()
-//                var container = try decoder.unkeyedContainer()
-//                let values = try container.dequeueReusableCell
-//                let jsonData =
-//                //let jsonData = try decoder.decode(ResponseData.self, from: data)
-//                return jsonData.person
-//            } catch {
-//                print("error:\(error)")
-//            }
-//        }
-//        return nil
-        
-//        if let url = Bundle.main.url(forResource: currencies, withExtension: "json") {
-//            do {
-//                let data = try Data(contentsOf: url)
-//                let decoder = JSONDecoder()
-//                decoder.unkeyedContainer
-//                //let decoderok = try decoder.unkeyedContainer()
-//                let jsonData = try decoder.decode(Currencies, from: data)
-//                return jsonData.person
-//            } catch {
-//                print("error:\(error)")
-//            }
-//        }
-        //return nil
-        
-
+        fetchRates()
+    }
+    
+    @objc func loop() {
+        if noRepeatControl {
+            timer.invalidate()
+        } else {
+            fetchRates()
+        }
     }
 
     func fetchRates() {
         
-        //rates = [:]
-        
         let userDefaults = UserDefaults.standard
-        let value = userDefaults.stringArray(forKey: "currencies_selected") ?? []
-        print(value)
+        let savedPairs = userDefaults.stringArray(forKey: "currencies_selected") ?? []
+        //print(savedPairs)
         
-        //let array = ["GBPUSD","GBPBRL","EURUSD","USDBRL"]
-        
+        if savedPairs.isEmpty {
+            print("Empty")
+            
+            
+        } else {
+            print("Not Empty")
+        }
+
         /// Call for the Service to fetch the items from the API
-        Service.shared.fetchItems(currencyPairs: value) { (items, error) in
+        Service.shared.fetchItems(currencyPairs: savedPairs) { (items, error) in
                    
                 /// Checking for errors after try to fetching the items
                 if let error = error {
@@ -139,44 +71,58 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
                 
                 if let items = items {
                     
-                    //guard let ite = items else { return }
-                    
-                    //self.rates = items
-                        
-                    //print(items)
-                    
-                    //self.itemsArray =  ite.items.map( { $0 } )
-
-                    self.rates = items.map( { $0.value } )
-                    
-                    self.currencyBase = items.map( { String($0.key.dropLast(3)) } )
-                    //self.currencyBase.dropLast(3)
-                    
-                    self.currencyOutput = items.map( { String($0.key.dropFirst(3)) } )
-                    //self.currencyOutput.dropFirst(3)
-                    
+                    print(savedPairs)
                     print(items)
-                    print(self.rates)
-                    print(self.currencyBase)
-                    print(self.currencyOutput)
-                    /// Sharing the ID of the item selected with other ViewController
-                    //let viewController = HomeViewController()
-                    //viewController.currencyPairs = items
                     
-                    /// Pushing a new ViewController
-                    //self.navigationController?.popToRootViewController(animated: true)
+                    //guard let nada = value.firstIndex(of: items ) else { return }
                     
-                    // .tableView.reloadData()
+                    //let ok = items.map( { $0.value } )
                     
-                    self.tableView.reloadData()
+                    let itemsFiltered = items.filter { item in
+                        savedPairs.contains(where: { $0 == item.key }
+                    )}
+
+                    print(itemsFiltered)
                     
-                    //self.myTableView.reloadData()
+                    let itemsFilteredSorted = itemsFiltered.sorted { savedPairs.firstIndex(of: $0.key)! < savedPairs.firstIndex(of: $1.key)! }
+                    
+                    print(itemsFilteredSorted)
+                       
+                    self.rates = itemsFilteredSorted.map( { $0.value } )
+                    self.currencyBase = itemsFilteredSorted.map( { String($0.key.dropLast(3)) } )
+                    self.currencyOutput = itemsFilteredSorted.map( { String($0.key.dropFirst(3)) } )
+                    
+                    if self.loadTableViewControl {
+                        self.tableView.reloadData()
+                        self.loadTableViewControl = false
+                    }
+                   
+                    var indexOfElement = 0
+                    
+                    for _ in self.currencyBase {
+
+                        let indexPathrow = IndexPath(item: indexOfElement, section: 0)
+                        print(indexPathrow)
+
+                        if let cell = self.tableView.cellForRow(at: indexPathrow) as? HomeViewCell {
+                            // Update the cell
+                            cell.rate.text = String(self.rates[indexPathrow.row])
+
+                        }
+                        indexOfElement += 1
+                    }
                 }
-                
         }
         
     }
 
+    @IBAction func addCurrencyPair(_ sender: UIButton) {
+        
+        noRepeatControl = true
+        
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.currencyBase.count
     }
@@ -185,54 +131,11 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! HomeViewCell
         
-        
-        //cell.currencyMain.text = "ok"
-        
         cell.currencyMain.text = currencyBase[indexPath.row]
         cell.currencyCompared.text = currencyOutput[indexPath.row]
         cell.rate.text = String(rates[indexPath.row])
         
-        //cell.currencyMainLabel.text = "ok"
-        //cell.currencyComparedLabel.text = "ok2"
-        //cell.rateLabel.text = "ok3"
-        
-        //cell.currencyMainLabel.text = rates[indexPath.row]
-        //    cell.currencyComparedLabel.text =
-        //    cell.rateLabel.text =
-        
-    //
-            
-            //cell.currencyName.text = "ok"
-            
-            //cell.currencyName.text = "ok"
-            
-            //cell.textLabel?.text = "ok"
-            
-            //currencyName.text = "ok"
-            
-            //currencyName.text = "ok"
-            
-            
-            //cell.contentView.te currencyName.text = ce
-    //        //let itemViewModel = itemViewModels[indexPath.row]
-    //        let item = itemsArray[indexPath.row]
-    //
-    //        cell.idLabel.text = String(item.id)
-    //        cell.titleLabel.text = item.title
-    //        cell.subtitleLabel.text = item.subtitle
-    //        cell.dateLabel.text = item.date
-    //
-            return cell
-    }
-        
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    //
-    //        let ok = itemsArray[indexPath.row].id
-    //
-    //        let vc = ItemDetailViewController()
-    //        vc.text = String(ok)
-    //
-    //        navigationController?.pushViewController(vc, animated: true)
+        return cell
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -240,8 +143,30 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
 
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let userDefaults = UserDefaults.standard
+        var value = userDefaults.stringArray(forKey: "currencies_selected") ?? []
+        
         if (editingStyle == .delete) {
-            // handle delete (by removing the data from your array and updating the tableview)
+           
+            print(indexPath)
+           
+            value.remove(at: indexPath.row)
+           
+            userDefaults.set(value, forKey: "currencies_selected")
+            
+            currencyBase.remove(at: indexPath.row)
+            currencyOutput.remove(at: indexPath.row)
+            rates.remove(at: indexPath.row)
+            
+            print(currencyBase)
+            print(currencyOutput)
+            print(rates)
+
+            tableView.deleteRows(at: [indexPath], with: .fade)
+                    
+            print(value)
+            self.tableView.reloadData()
         }
     }
 }
